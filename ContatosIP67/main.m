93e2cce4af7ad76fcf975241740c743d5466ecbc
ios3401 <ios3401@caelum57-01.local>
2013-03-26 19:14:26 -0300

    Initial Commit

---
+++ ContatosIP67/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ContatosIP67
+//
+//  Created by ios3401 on 26/03/13.
+//  Copyright (c) 2013 br.com.caelum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EFAppDelegate class]));
+    }
+}
