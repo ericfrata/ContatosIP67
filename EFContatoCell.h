fabc022c3c2cd474d74d0ee0cde999aeffaa0518
ericfrata <er.frata@gmail.com>
2013-04-03 21:00:01 -0300

    Custom cell

---
+++ EFContatoCell.h
@@ -0,0 +1,13 @@
+//
+//  EFContatoCell.h
+//  ContatosIP67
+//
+//  Created by ios3401 on 03/04/13.
+//  Copyright (c) 2013 br.com.caelum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EFContatoCell : UITableViewCell
+
+@end
---
+++ EFContatoCell.m
@@ -0,0 +1,29 @@
+//
+//  EFContatoCell.m
+//  ContatosIP67
+//
+//  Created by ios3401 on 03/04/13.
+//  Copyright (c) 2013 br.com.caelum. All rights reserved.
+//
+
+#import "EFContatoCell.h"
+
+@implementation EFContatoCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
